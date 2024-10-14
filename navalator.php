298a902ce4aef5384fa226fc131c1c3c55b02c40
courumix <courumix@gmail.com>
2024-10-14 12:09:26 +0200

    modif des fonctions de positions et début du code de jeu

--- navalator.php
+++ navalator.php
@@ -30,38 +30,58 @@ function tire($l,$c,&$grille){
 }
 
 function positionnetorpilleur(&$grille){
-    $l = (int)readline("Sur quelle ligne voulez vous positionner le torpilleur ? : ") -1;
-    $c = (int)readline("Sur quelle colonne voulez vous positionner le torpilleur ? : ") -1;
-    $position = readline("voulez-vous postionner le torpilleur en horizontal ? (o/n) : ");
-    if($position == "o"){
-        if($grille[$l][$c] == "." and $grille[$l][$c+1] == "."){
-            $grille[$l][$c] = "H";
-            $grille[$l][$c+1] = "T";
+    $placé = false;
+    while ($placé == false){
+        $l = (int)readline("Sur quelle ligne voulez vous positionner le torpilleur ? : ") -1;
+        $c = (int)readline("Sur quelle colonne voulez vous positionner le torpilleur ? : ") -1;
+        $position = "none";
+        while($position != "o" and $position != "n"){
+            $position = readline("voulez-vous postionner le torpilleur en horizontal ? (o/n) : ");
+            if($position != "o" and $position != "n"){
+            echo"vous devez entrez o ou n \n";
+            }
         }
-        else{
-            echo "vous ne pouvez pas placer votre bateau ici\n";
+        if($position == "o"){
+            if($grille[$l][$c] == "." and $grille[$l][$c+1] == "."){
+                $grille[$l][$c] = "H";
+                $grille[$l][$c+1] = "T";
+                $placé = true;
+            }
+            else{
+                echo "vous ne pouvez pas placer votre bateau ici\n";
+            }
         }
-    }
-    elseif($position == "n"){
-        if($grille[$l][$c] == "." and $grille[$l+1][$c] == "."){
-            $grille[$l][$c] = "H";
-            $grille[$l+1][$c] = "T";
-        }
-        else{
-            echo "vous ne pouvez pas placer votre bateau ici\n";
+        elseif($position == "n"){
+            if($grille[$l][$c] == "." and $grille[$l+1][$c] == "."){
+                $grille[$l][$c] = "H";
+                $grille[$l+1][$c] = "T";
+                $placé = true;
+            }
+            else{
+                echo "vous ne pouvez pas placer votre bateau ici\n";
+            }
         }
     }
 }
 
 function positionnecontretorpilleur(&$grille){
+    $placé = false;
+    while($placé == false){
     $l = (int)readline("Sur quelle ligne voulez vous positionner le contre-torpilleur ? : ") -1;
     $c = (int)readline("Sur quelle colonne voulez vous positionner le contre-torpilleur ? : ") -1;
-    $position = readline("voulez-vous postionner le contre-torpilleur en horizontal ? (o/n) : ");
+    $position = "none";
+    while($position != "o" and $position != "n"){
+        $position = readline("voulez-vous postionner le torpilleur en horizontal ? (o/n) : ");
+        if($position != "o" and $position != "n"){
+        echo"vous devez entrez o ou n\n";
+        }
+    }
     if($position == "o"){
         if($grille[$l][$c] == "." and $grille[$l][$c+1] == "." and $grille[$l][$c+2] == "."){
             $grille[$l][$c] = "H";
             $grille[$l][$c+1] = "c";
             $grille[$l][$c+2] = "c";
+            $placé = true;
         }
         else{
             echo "vous ne pouvez pas placer votre bateau ici\n";
@@ -72,23 +92,34 @@ function positionnecontretorpilleur(&$grille){
             $grille[$l][$c] = "H";
             $grille[$l+1][$c] = "c";
             $grille[$l+2][$c] = "c";
+            $placé = true;
         }
         else{
             echo "vous ne pouvez pas placer votre bateau ici\n";
         }
     }
+    }
 }
 
 function positionnecroiseur(&$grille){
+    $placé = false;
+    while($placé == false){
     $l = (int)readline("Sur quelle ligne voulez vous positionner le croiseur ? : ") -1;
     $c = (int)readline("Sur quelle colonne voulez vous positionner le croiseur ? : ") -1;
-    $position = readline("voulez-vous postionner le croiseur en horizontal ? (o/n) : ");
+    $position = "none";
+    while($position != "o" and $position != "n"){
+        $position = readline("voulez-vous postionner le torpilleur en horizontal ? (o/n) : ");
+        if($position != "o" and $position != "n"){
+        echo"vous devez entrez o ou n\n";
+        }
+    }
     if($position == "o"){
         if($grille[$l][$c] == "." and $grille[$l][$c+1] == "." and $grille[$l][$c+2] == "." and $grille[$l][$c+3] == "."){
             $grille[$l][$c] = "H";
             $grille[$l][$c+1] = "C";
             $grille[$l][$c+2] = "C";
             $grille[$l][$c+3] = "C";
+            $placé = true;
         }
         else{
             echo "vous ne pouvez pas placer votre bateau ici\n";
@@ -100,17 +131,27 @@ function positionnecroiseur(&$grille){
             $grille[$l+1][$c] = "C";
             $grille[$l+2][$c] = "C";
             $grille[$l+3][$c] = "C";
+            $placé = true;
         }
         else{
             echo "vous ne pouvez pas placer votre bateau ici\n";
         }
     }
+    }
 }
 
 function positionneporteavion(&$grille){
+    $placé = false;
+    while($placé == false){
     $l = (int)readline("Sur quelle ligne voulez vous positionner le porte avion ? : ") -1;
     $c = (int)readline("Sur quelle colonne voulez vous positionner le porte avion ? : ") -1;
-    $position = readline("voulez-vous postionner le porte avion en horizontal ? (o/n) : ");
+    $position = "none";
+    while($position != "o" and $position != "n"){
+        $position = readline("voulez-vous postionner le torpilleur en horizontal ? (o/n) : ");
+        if($position != "o" and $position != "n"){
+        echo"vous devez entrez o ou n\n";
+        }
+    }
     if($position == "o"){
         if($grille[$l][$c] == "." and $grille[$l][$c+1] == "." and $grille[$l][$c+2] == "." and $grille[$l][$c+3] == "." and $grille[$l][$c+4] == "."){
             $grille[$l][$c] = "H";
@@ -118,6 +159,7 @@ function positionneporteavion(&$grille){
             $grille[$l][$c+2] = "P";
             $grille[$l][$c+3] = "P";
             $grille[$l][$c+4] = "P";
+            $placé = true;
         }
         else{
             echo "vous ne pouvez pas placer votre bateau ici\n";
@@ -130,11 +172,53 @@ function positionneporteavion(&$grille){
             $grille[$l+2][$c] = "P";
             $grille[$l+3][$c] = "P";
             $grille[$l+4][$c] = "P";
+            $placé = true;
         }
         else{
             echo "vous ne pouvez pas placer votre bateau ici\n";
         }
     }
+    }
+}
+
+$jeu = true;
+
+while ($jeu){
+    $TabJ1 = initialisegrille();
+    $TabJ2 = initialisegrille();
+    echo"voici le tableau du J1 : \n";
+    affichetab($TabJ1,10,10);
+    echo"voici le tableu du J2 : \n";
+    affichetab($TabJ2,10,10);
+    readline("J1 tapez sur entrez quand vous êtes prêts à installer vos bateaux (J2 ne doit pas regarder) : \n");
+    positionnetorpilleur($TabJ1);
+    echo"voici votre plan de jeu actuel : \n";
+    affichetab($TabJ1,10,10);
+    positionnecontretorpilleur($TabJ1);
+    echo"voici votre plan de jeu actuel : \n";
+    affichetab($TabJ1,10,10);
+    positionnecroiseur($TabJ1);
+    echo"voici votre plan de jeu actuel : \n";
+    affichetab($TabJ1,10,10);
+    positionneporteavion($TabJ1);
+    echo"voici votre plan de jeu actuel : \n";
+    affichetab($TabJ1,10,10);
+    readline("J1 tapez sur la barre espace pour laisser la place à J2 : \n");
+    echo".\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n";
+    readline("J2 tapez sur entrez quand vous êtes prêts à installer vos bateaux (J1 ne doit pas regarder) : \n");
+    positionnetorpilleur($TabJ2);
+    echo"voici votre plan de jeu actuel : \n";
+    affichetab($TabJ1,10,10);
+    positionnecontretorpilleur($TabJ2);
+    echo"voici votre plan de jeu actuel : \n";
+    affichetab($TabJ1,10,10);
+    positionnecroiseur($TabJ2);
+    echo"voici votre plan de jeu actuel : \n";
+    affichetab($TabJ1,10,10);
+    positionneporteavion($TabJ2);
+    echo"voici votre plan de jeu actuel : \n";
+    affichetab($TabJ1,10,10);
+    $jeu = false;
 }
 
 #$grilleJ1 = initialisegrille();
